a85711557934873372340611b8c1438cd64f9409
EXCEEDSYSTEM <contact@exceedsystem.com>
2023-10-22 19:00:20 +0900

    Initial version

---
+++ src/extension.ts
@@ -0,0 +1,88 @@
+import { spawn } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+const asyncFs = fs.promises;
+
+const OPTION_LABEL: string = '# Options ';
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand('pict.run', async () => {
+    if (!vscode.window.activeTextEditor || !vscode.window.activeTextEditor.document) {
+      await vscode.window.showErrorMessage('Document is not open.');
+      return;
+    }
+    const textEditor = vscode.window.activeTextEditor;
+    const document = textEditor?.document;
+
+    if (!document?.fileName || !fs.existsSync(document?.fileName) || document?.isDirty) {
+      await vscode.window.showErrorMessage('Please save the document before running pict.');
+      return;
+    }
+
+    const inputFilePath = document?.fileName;
+    const workingDirPath = path.dirname(inputFilePath);
+    process.chdir(workingDirPath);
+
+    const eol = document.eol === vscode.EndOfLine.LF ? '\r' : '\r\n';
+
+    const editorText = (await asyncFs.readFile(inputFilePath!)).toString();
+    const pictParams = (
+      editorText
+        .split(eol)
+        .filter((s) => s.startsWith(OPTION_LABEL))
+        .at(0)
+        ?.substring(OPTION_LABEL.length) ?? ''
+    )
+      .replace(/(?=\/[odanercs][:]*[^\/]*[odanercs]*)/g, '\t')
+      .split('\t')
+      .map((s) => s.trim())
+      .filter((s) => s);
+
+    const pictExecutablePath = context.asAbsolutePath(path.join('resources', 'pict.exe'));
+    let pictRet: SpawnRet = { stdout: '', error: '' };
+    try {
+      pictRet = await asyncSpawn(pictExecutablePath, [inputFilePath, ...pictParams]);
+    } catch (e) {
+      await vscode.window.showErrorMessage(`An error ocurred during running pict.(${(e as SpawnRet).error})`);
+      return;
+    }
+
+    const testcaseText = pictRet.stdout;
+
+    const newDoc = await vscode.workspace.openTextDocument({ language: 'tsv', content: testcaseText });
+
+    await vscode.window.showTextDocument(newDoc);
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
+
+async function asyncSpawn(command: string, args: string[]): Promise<SpawnRet> {
+  return new Promise((resolve, reject) => {
+    const cp = spawn(command, args);
+    let stdout = '';
+    let error = '';
+    cp.stdout.on('data', (d) => {
+      stdout += d.toString();
+    });
+    cp.stderr.on('data', (e) => {
+      error += e.toString();
+    });
+    cp.on('close', (c) => {
+      if (c === 0) {
+        resolve({ stdout, error });
+      } else {
+        reject({ stdout, error });
+      }
+    });
+  });
+}
+
+interface SpawnRet {
+  stdout: string;
+  error: string;
+}
